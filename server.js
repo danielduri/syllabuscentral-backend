ba8880641b08522d102297c5ebffca4569aa3c43
danielduri <danielduri@gmail.com>
2022-11-24 01:46:03 +0100

    connect to db with connectionstring

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
