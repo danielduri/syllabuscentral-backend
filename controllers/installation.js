4952193ae870348d81b65c1fe1029753b5bad57b
danielduri <danielduri@gmail.com>
2023-03-01 19:54:15 +0100

    installation fix

--- controllers/installation.js
+++ controllers/installation.js
@@ -3,42 +3,42 @@ import {getDepartmentNameFromID, getUserNameFromID} from "../functions/nameGette
 
 export async function installation(req, res, db, bcrypt) {
 
-    const school0 = await getSchoolNameFromID(0, db)
-    if (school0 === undefined) {
+    const school1 = await getSchoolNameFromID(1, db)
+    if (school1 === undefined) {
         await db("schools").insert({
-            schoolID: 0,
+            schoolID: 1,
             schoolName: "Administración Central"
         }).then(() => {
-            console.log("Created school 0")
+            console.log("Created school 1")
         })
     }
 
-    const dept0 = await getDepartmentNameFromID(0, db)
-    if (dept0 === undefined) {
+    const dept1 = await getDepartmentNameFromID(1, db)
+    if (dept1 === undefined) {
         await db("departments").insert({
-            departmentID: 0,
+            departmentID: 1,
             departmentName: "Departamento Administración",
             departmentShorthand: "ADM",
-            departmentSchoolID: 0
+            departmentSchoolID: 1
         }).then(() => {
-            console.log("Created department 0")
+            console.log("Created department 1")
         })
     }
 
-    const user0 = await getUserNameFromID(0, db)
-    if (user0 === undefined) {
+    const user1 = await getUserNameFromID(1, db)
+    if (user1 === undefined) {
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash("pass", salt);
         await db("users").insert({
-            userID: 0,
+            userID: 1,
             email: "super@syllabus.com",
             userName: "admin",
             passwordHash: hash,
             userType: 2,
-            schoolID: 0,
-            departmentID: 0
+            schoolID: 1,
+            departmentID: 1
         }).then(() => {
-            console.log("Created user 0")
+            console.log("Created user 1")
         })
     }
 
