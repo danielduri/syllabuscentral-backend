809cf98160177eb58101aee661f764a5833b55d1
danielduri <danielduri@gmail.com>
2023-03-01 19:01:45 +0100

    cors fix 1

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ import {installation} from "./controllers/installation.js";
 import {promoteUser} from "./controllers/identityManagement/promoteUser.js";
 
 const app = express();
+app.use(cors())
 
 let db = knex({
     client: 'pg',
@@ -91,7 +92,6 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 
-app.use(cors())
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 
