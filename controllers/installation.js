25b45ab0f71e85fd349c3ac553f334d02069d34d
danielduri <danielduri@gmail.com>
2023-03-01 20:38:29 +0100

    fix installation.js

--- controllers/installation.js
+++ controllers/installation.js
@@ -4,26 +4,31 @@ import {getDepartmentNameFromID, getUserNameFromID} from "../functions/nameGette
 export async function installation(req, res, db, bcrypt) {
 
     const school1 = await getSchoolNameFromID(1, db)
+    let school1ID
     if (school1 === undefined) {
         await db("schools").insert({
             schoolName: "Administración Central"
-        }).then(() => {
+        }).returning("schoolID").then((schoolID) => {
+            school1ID = schoolID.schoolID
             console.log("Created school 1")
         })
     }
 
     const dept1 = await getDepartmentNameFromID(1, db)
+    let dept1ID
     if (dept1 === undefined) {
         await db("departments").insert({
             departmentName: "Departamento Administración",
             departmentShorthand: "ADM",
-            departmentSchoolID: 1
-        }).then(() => {
+            departmentSchoolID: school1ID
+        }).returning("departmentID").then(() => {
+            dept1ID = departmentID.departmentID
             console.log("Created department 1")
         })
     }
 
     const user1 = await getUserNameFromID(1, db)
+    let user1ID
     if (user1 === undefined) {
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash("pass", salt);
@@ -32,9 +37,10 @@ export async function installation(req, res, db, bcrypt) {
             userName: "admin",
             passwordHash: hash,
             userType: 2,
-            schoolID: 1,
-            departmentID: 1
-        }).then(() => {
+            schoolID: school1ID,
+            departmentID: dept1ID
+        }).returning("userID").then(() => {
+            user1ID = userID.userID
             console.log("Created user 1")
         })
     }
